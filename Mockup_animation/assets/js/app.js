7d4d84c359532626d698691ca159803e659aeec1
Olga <miliukovao@gmail.com>
2022-12-03 18:36:30 +0200

    final mockup

---
+++ Mockup_animation/assets/js/app.js
@@ -0,0 +1,16 @@
+// $('.multiple-items').slick({
+//     infinite: true,
+//     autoplaySpeed: 1500,
+// });
+
+
+$(function() {
+    $('.goat_block').slick({
+        prevArrow: false,
+        nextArrow: false,
+        vertical: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    })
+});
